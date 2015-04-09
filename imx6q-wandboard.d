8d52fd00546ca91c4ba1b281bc109c15e524b8ce
Wes Malone <wes@mitsi.com>
2015-04-09 10:22:04 -0500

    remove gcc intermediate file

--- imx6q-wandboard.d
+++
@@ -1,4 +0,0 @@
-imx6q-wandboard.o: imx6q-wandboard.dts \
- include/dt-bindings/interrupt-controller/irq.h imx6q.dtsi \
- imx6q-pinfunc.h imx6qdl.dtsi skeleton.dtsi \
- include/dt-bindings/gpio/gpio.h imx6qdl-wandboard.dtsi
